88f0f06418aadd680b24d645c039b9574a0cc3aa
jmhans <jmhans@hotmail.com>
2026-03-06 08:47:24 -0600

    Make all ALTER TABLE ADD COLUMN migrations idempotent with DO blocks

--- drizzle/0001_true_eddie_brock.sql
+++ drizzle/0001_true_eddie_brock.sql
@@ -1,1 +1,5 @@
-ALTER TABLE "ampf1"."bingo_events" ADD COLUMN "is_achieved" boolean DEFAULT false NOT NULL;
+DO $$ BEGIN
+  IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='bingo_events' AND column_name='is_achieved') THEN
+    ALTER TABLE "ampf1"."bingo_events" ADD COLUMN "is_achieved" boolean DEFAULT false NOT NULL;
+  END IF;
+END $$;
--- drizzle/0003_race_start_time.sql
+++ drizzle/0003_race_start_time.sql
@@ -1,1 +1,5 @@
-ALTER TABLE "ampf1"."races" ADD COLUMN "race_start_time" timestamp;
+DO $$ BEGIN
+  IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='races' AND column_name='race_start_time') THEN
+    ALTER TABLE "ampf1"."races" ADD COLUMN "race_start_time" timestamp;
+  END IF;
+END $$;
